aa52e284eaeecc369ae591c2279d1094dd602ba9
Sankar110000 <skaibalya748@gmail.com>
2026-06-21 18:14:52 +0530

    added host

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0",  () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
